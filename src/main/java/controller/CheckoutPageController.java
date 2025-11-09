4eee9534f22bf2b60b778cdd3a518e52a877ab5e
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-09 21:08:44 +0530

    Create check out dashboard ui

---
+++ src/main/java/controller/CheckoutPageController.java
@@ -0,0 +1,103 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CheckoutPageController {
+
+    Stage stage = new Stage();
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnPay;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private TableView<?> tblCart;
+
+    @FXML
+    private JFXTextField txtCode;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    void btnAddOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackOnaction(ActionEvent event) throws IOException {
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"))));
+        stage.setResizable(false);
+        stage.setTitle("Admin");
+        stage.show();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void btnDeleteOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPayOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnaction(ActionEvent event) {
+
+    }
+
+}
