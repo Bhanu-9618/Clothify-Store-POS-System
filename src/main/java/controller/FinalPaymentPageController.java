966855fde5f8db76df7ee92e78a82ef8325bb403
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-14 17:06:47 +0530

    Update payment ui

--- src/main/java/controller/FinalPaymentPageController.java
+++ src/main/java/controller/FinalPaymentPageController.java
@@ -3,10 +3,17 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class FinalPaymentPageController {
+
+    Stage stage = new Stage();
 
     @FXML
     private JFXButton btnConfirm;
@@ -27,8 +34,30 @@ public class FinalPaymentPageController {
     private TextField txtDiscount;
 
     @FXML
-    void btnConfirmOnaction(ActionEvent event) {
+    private JFXButton btnBack;
 
+    @FXML
+    void btnConfirmOnaction(ActionEvent event) throws IOException {
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CheckOutPage.fxml"))));
+        stage.setResizable(false);
+        stage.setTitle("Checkout");
+        stage.show();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void btnBackOnaction(ActionEvent event) throws IOException {
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CheckOutPage.fxml"))));
+        stage.setResizable(false);
+        stage.setTitle("Checkout");
+        stage.show();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 
 }
