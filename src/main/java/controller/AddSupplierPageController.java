7fa464c273120b627b65e5b5c41249ea73a21e7f
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-21 11:58:52 +0530

    create add supplier ui

---
+++ src/main/java/controller/AddSupplierPageController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddSupplierPageController {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnAddOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackOnaction(ActionEvent event) {
+
+    }
+
+}
