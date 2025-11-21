9674bc5ab1f62ac6b7916a8e4abe412d3e1eec9c
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-21 12:17:16 +0530

    create UIs for update and delete supplier

---
+++ src/main/java/controller/DeleteSupplierPageController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DeleteSupplierPageController {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    void btnBackOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+}
