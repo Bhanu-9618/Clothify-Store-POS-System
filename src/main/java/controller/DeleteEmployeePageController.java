a0904e18379c5d9b954140da64ecff5c33c881c6
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-21 11:38:48 +0530

    create delete employee ui

---
+++ src/main/java/controller/DeleteEmployeePageController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DeleteEmployeePageController {
+
+    Stage stage = new Stage();
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private TextField txtDeleteID;
+
+    @FXML
+    void btnBackOnaction(ActionEvent event) throws IOException {
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"))));
+        stage.setResizable(false);
+        stage.setTitle("Admin");
+        stage.show();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void btnDeleteOnaction(ActionEvent event) {
+
+    }
+
+}
